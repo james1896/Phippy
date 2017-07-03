8597e9be4fbc72a897dd814691feb08ea6a12010
james <james@gmail.com>
2017-07-03 20:26:38 +0800

    完善

--- app/src/main/java/com/tiffany/phippy/life/LifeFragment.java
+++ app/src/main/java/com/tiffany/phippy/life/LifeFragment.java
@@ -10,7 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
@@ -35,60 +37,15 @@ public class LifeFragment extends Fragment {
 
         contentView = inflater.inflate(R.layout.fragment_life, container, false);
 
-        Button btn = (Button) contentView.findViewById(R.id.aa);
-        btn.setOnClickListener(new View.OnClickListener() {
+        //小娘子药铺
+        ImageView xiaoniangzi = (ImageView) contentView.findViewById(R.id.life_medicine);
+        xiaoniangzi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(getActivity(),MedicineActivity.class);
                 startActivity(intent);
             }
         });
-//        View  banner_view = inflater.inflate(R.layout.medicine_banner, container, false);
-//        convenientBanner = (ConvenientBanner) banner_view.findViewById(R.id.convenientBanner);
-//        //获取本地的图片
-//        for (int position = 0; position < 3; position++) {
-//            localImages.add(getResId("ic_test_" + position, R.mipmap.class));
-//        }
-//
-////开始自动翻页
-//        convenientBanner.setPages(new CBViewHolderCreator() {
-//            @Override
-//            public Object createHolder() {
-//                return new LocalImageHolderView();
-//            }
-//        },localImages)
-//                //设置指示器是否可见
-//                .setPointViewVisible(true)
-//                //设置自动切换（同时设置了切换时间间隔）
-//                .startTurning(2000)
-//                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-////                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
-//                //设置指示器的方向（左、中、右）
-//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
-//                //设置点击监听事件
-////                .setOnItemClickListener(this)
-//                //设置手动影响（设置了该项无法手动切换）
-//                .setManualPageable(true);
-//
-//
-//        //设置翻页的效果，不需要翻页效果可用不设
-//        //setPageTransformer(Transformer.DefaultTransformer);   // 集成特效之后会有白屏现象，新版已经分离，如果要集成特效的例子可以看Demo的点击响应。
-//
-//
-//
-//        ListView listView = (ListView) contentView.findViewById(R.id.medicine_listView);
-//        listView.setAdapter(new MedicineAdapter(getContext(),null));
-//        listView.addHeaderView(convenientBanner);
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//
-//                Intent intent = new Intent(getActivity(),MedicineListActivity.class);
-//                intent.putExtra("com.tiffany.android_intent.age", i);
-//                startActivity(intent);
-//            }
-//        });
         return contentView;
     }
 
