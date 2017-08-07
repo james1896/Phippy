8e15ca57646f1b575a97802dd3ff66f5d3f47f97
james <james@gmail.com>
2017-08-07 16:17:29 +0800

    我的  ui调整

--- app/src/main/java/com/tiffany/phippy/me/MeListAdapter.java
+++ app/src/main/java/com/tiffany/phippy/me/MeListAdapter.java
@@ -3,6 +3,7 @@ package com.tiffany.phippy.me;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.tiffany.phippy.R;
@@ -17,7 +18,7 @@ import java.util.Map;
  * Created by moses on 27/06/2017.
  */
 
-public class MeListAdapter extends BaseListAdapter1 {
+public class MeListAdapter extends BaseListAdapter {
 
 
 //    protected ArrayList<Map> dataList;
@@ -32,10 +33,10 @@ public class MeListAdapter extends BaseListAdapter1 {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        Map<String,Object> map = this.dataList.get(position);
+        ArrayList<Map> list = (ArrayList<Map>) this.dataList.get(position);
 
         View item = mInflater.inflate(R.layout.me_listview_item, null);
-        TextView titleTV = (TextView)item.findViewById(R.id.me_item_title);
+//        TextView titleTV = (TextView)item.findViewById(R.id.me_item_title);
 //        TextView addressTV = (TextView)item.findViewById(R.id.food_list_item_address);
 //        TextView current_priceTV = (TextView)item.findViewById(R.id.food_list_item_price);
 //        TextView original_priceTV = (TextView)item.findViewById(R.id.food_list_item_price1);
@@ -44,14 +45,14 @@ public class MeListAdapter extends BaseListAdapter1 {
 
 //        ImageView img = (ImageView)item.findViewById(R.id.imageView);
 
-        String titleStr = (String) map.get("title");
+//        String titleStr = (String) map.get("title");
 //        String addressStr = (String) map.get("add");
 //        String current_priceStr = (String) map.get("current_price");
 //        String original_priceStr = (String) map.get("original_price");
 //        String hotStr = (String) map.get("hot");
 
 
-        titleTV.setText(titleStr);
+//        titleTV.setText(titleStr);
 //        addressTV.setText(addressStr);
 //        current_priceTV.setText(current_priceStr);
 //        original_priceTV.setText(original_priceStr);
@@ -59,59 +60,87 @@ public class MeListAdapter extends BaseListAdapter1 {
 
 //        int resid = (int) map.get("resId");
 //        img.setImageResource(resid);
+
+        ListView lv = (ListView) item.findViewById(R.id.me_item_listview);
+        lv.setAdapter(new MeListItemAdapter(context,list));
         return item;
     }
 
-    public ArrayList<Map> getDataList(){
-        ArrayList<Map> list = new ArrayList<>();
+    public ArrayList<ArrayList> getDataList(){
+        ArrayList<ArrayList> list = new ArrayList<>();
 
+        ArrayList<Map> list1 = new ArrayList<>();
         Map<String,Object> map1 = new HashMap<String,Object>();
-        map1.put("title","我的收藏");
+        map1.put("title","我的订单");
 //        map1.put("resId",R.drawable.me_list_zengsong);
 
-        list.add(map1);
+        list1.add(map1);
 
+        ArrayList<Map> list2 = new ArrayList<>();
         Map<String,Object> map2 = new HashMap<String,Object>();
         map2.put("title","应急求助");
 
-        list.add(map2);
+        list2.add(map2);
 
 
         Map<String,Object> map3 = new HashMap<String,Object>();
         map3.put("title","常用设置");
-        list.add(map3);
+        list2.add(map3);
 
         Map<String,Object> map4 = new HashMap<String,Object>();
         map4.put("title","意见反馈");
 
-        list.add(map4);
+        list2.add(map4);
 //
 
         Map<String,Object> map5 = new HashMap<String,Object>();
         map5.put("title","商务合作");
 
-        list.add(map5);
+        list2.add(map5);
 
         Map<String,Object> map6 = new HashMap<String,Object>();
         map6.put("title","关于我们");
 
-        list.add(map6);
+        list2.add(map6);
 
+        ArrayList<Map> list3 = new ArrayList<>();
         Map<String,Object> map7 = new HashMap<String,Object>();
         map7.put("title","退出登录");
 
-        list.add(map7);
+        list3.add(map7);
 
         Map<String,Object> map8 = new HashMap<String,Object>();
         map8.put("title","DBDebug");
-        list.add(map8);
+        list3.add(map8);
 
 
-
-
+        list.add(list1);
+        list.add(list2);
+        list.add(list3);
 
 
         return list;
 
     }
+
+
+}
+
+class MeListItemAdapter extends BaseListAdapter{
+
+    public MeListItemAdapter(Context context, ArrayList list) {
+        super(context, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Map<String,Object> map = (Map<String, Object>) this.dataList.get(position);
+
+        View item = mInflater.inflate(R.layout.me_listview_item_item, null);
+        TextView titleTV = (TextView)item.findViewById(R.id.me_item_title);
+        titleTV.setText((String) map.get("title"));
+        return item;
+    }
+
 }
