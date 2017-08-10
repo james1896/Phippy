b37ca56667da9e872fe5468be7903795f293b0fa
james <james@gmail.com>
2017-08-10 19:47:11 +0800

    收货地址

--- app/src/main/java/com/tiffany/phippy/me/myAddress/MyAddressActivity.java
+++ app/src/main/java/com/tiffany/phippy/me/myAddress/MyAddressActivity.java
@@ -1,5 +1,8 @@
 package com.tiffany.phippy.me.myAddress;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.tiffany.phippy.R;
@@ -14,6 +17,8 @@ import java.util.Map;
  */
 
 public class MyAddressActivity extends BaseActivity {
+    ArrayList<Map> dataList = new ArrayList<>();
+    MyAddressListAdapter adapter;
     @Override
     protected int getContentView() {
         return R.layout.activity_me_myaddress;
@@ -23,31 +28,85 @@ public class MyAddressActivity extends BaseActivity {
     protected void init() {
 
         setToolbarTitle("收货地址");
+        this.dataList = getDataList();
+        this.adapter = new MyAddressListAdapter(this,this.dataList);
         ListView lv = (ListView) findViewById(R.id.myaddress_listview);
-        lv.setAdapter(new MyAddressListAdapter(this,getDataList()));
+        lv.setAdapter(this.adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.e("address",""+i);
+//                Map<String,Object> map = dataList.get(i);
+//                map.put("selected",true);
+
+//                Map<String,Object> map1 = dataList.get(0);
+//                map1.put("selected",false);
+
+                for(int y=0;y<dataList.size();y++){
+
+                    Map<String,Object> tmpMap = dataList.get(y);
+
+                    boolean flog = (boolean) tmpMap.get("selected");
+                    if(flog){
+                        tmpMap.put("selected",false);
+                        break;
+                    }
+                }
+
+                Log.e("address","size:"+dataList.size());
+                Map<String,Object> map = dataList.get(i);
+                map.put("selected",true);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     ArrayList<Map> getDataList(){
         ArrayList<Map> list = new ArrayList<>();
 
         Map<String,Object> map1 = new HashMap<String, Object>();
+        map1.put("selected",true);
         map1.put("name","james");
         map1.put("phone","09162511897");
         map1.put("address","antel spa hotel makati manila phidela costa evune antel sopa hotel dela costa address");
 
         Map<String,Object> map2 = new HashMap<String, Object>();
+        map2.put("selected",false);
         map2.put("name","james");
         map2.put("phone","09162511897");
         map2.put("address","antel spa hotel makati manila phi");
 
         Map<String,Object> map3 = new HashMap<String, Object>();
+        map3.put("selected",false);
         map3.put("name","james");
         map3.put("phone","09162511897");
         map3.put("address","antel spa hotel makati manila phi");
 
+        Map<String,Object> map4 = new HashMap<String, Object>();
+        map4.put("selected",false);
+        map4.put("name","james");
+        map4.put("phone","09162511897");
+        map4.put("address","antel spa hotel makati manila phi");
+
+        Map<String,Object> map5 = new HashMap<String, Object>();
+        map5.put("selected",false);
+        map5.put("name","james");
+        map5.put("phone","09162511897");
+        map5.put("address","antel spa hotel makati manila phi");
+
+        Map<String,Object> map6 = new HashMap<String, Object>();
+        map6.put("selected",false);
+        map6.put("name","james");
+        map6.put("phone","09162511897");
+        map6.put("address","antel spa hotel makati manila phi");
         list.add(map1);
         list.add(map2);
         list.add(map3);
+        list.add(map4);
+        list.add(map5);
+        list.add(map6);
+
         return list;
     }
 }
--- app/src/main/java/com/tiffany/phippy/me/myAddress/MyAddressListAdapter.java
+++ app/src/main/java/com/tiffany/phippy/me/myAddress/MyAddressListAdapter.java
@@ -1,9 +1,12 @@
 package com.tiffany.phippy.me.myAddress;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tiffany.phippy.PHIUserManager;
@@ -27,8 +30,6 @@ public class MyAddressListAdapter extends BaseListAdapter1 {
 
     }
 
-
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
@@ -43,26 +44,65 @@ public class MyAddressListAdapter extends BaseListAdapter1 {
             holder.name = (TextView) convertView.findViewById(R.id.myaddress_item_name);
             holder.phoneNumber = (TextView) convertView.findViewById(R.id.myaddress_item_phone);
             holder.address = (TextView) convertView.findViewById(R.id.myaddress_item_address);
-            holder.cancel = (Button) convertView.findViewById(R.id.myaddress_item_cancel);
-            holder.edit = (Button) convertView.findViewById(R.id.myaddress_item_edit);
+            holder.cancel = (TextView) convertView.findViewById(R.id.myaddress_item_cancel);
+            holder.edit = (TextView) convertView.findViewById(R.id.myaddress_item_edit);
+            holder.hline = (LinearLayout) convertView.findViewById(R.id.myaddress_item_hline);
+            holder.vline = (LinearLayout) convertView.findViewById(R.id.myaddress_item_vline);
+
+            holder.check = (ImageView) convertView.findViewById(R.id.myaddress_item_check);
             convertView.setTag(holder);
         }else {
             holder = (ViewHolder) convertView.getTag();
         }
 
+        boolean selected = (boolean) map.get("selected");
+        if(selected){
+            holder.check.setVisibility(View.VISIBLE);
+            setSelectedColor(holder);
+        }else {
+            holder.check.setVisibility(View.INVISIBLE);
+            setUnselectedColor(holder);
+
+        }
         holder.name.setText((String) map.get("name"));
         holder.phoneNumber.setText((String) map.get("phone"));
         holder.address.setText((String) map.get("address"));
         return convertView;
     }
 
+    private void setSelectedColor(ViewHolder holder){
+
+        holder.name.setTextColor(Color.parseColor("#696969"));
+        holder.phoneNumber.setTextColor(Color.parseColor("#696969"));
+        holder.address.setTextColor(Color.parseColor("#696969"));
+        holder.cancel.setTextColor(Color.parseColor("#696969"));
+        holder.edit.setTextColor(Color.parseColor("#696969"));
+        holder.hline.setBackgroundColor(Color.parseColor("#696969"));
+        holder.vline.setBackgroundColor(Color.parseColor("#696969"));
+    }
+
+    private void setUnselectedColor(ViewHolder holder){
+        holder.name.setTextColor(Color.parseColor("#D3D3D3"));
+        holder.phoneNumber.setTextColor(Color.parseColor("#D3D3D3"));
+        holder.address.setTextColor(Color.parseColor("#D3D3D3"));
+        holder.cancel.setTextColor(Color.parseColor("#D3D3D3"));
+        holder.edit.setTextColor(Color.parseColor("#D3D3D3"));
+
+        holder.hline.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        holder.vline.setBackgroundColor(Color.parseColor("#D3D3D3"));
+    }
+
     public class ViewHolder{
+        ImageView check;
         TextView name;
         TextView phoneNumber;
         TextView address;
 
-        Button cancel;
-        Button edit;
+        TextView cancel;
+        TextView edit;
+
+        LinearLayout hline;
+        LinearLayout vline;
     }
 
 }
